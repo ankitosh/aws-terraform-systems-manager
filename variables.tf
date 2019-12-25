6175906d7192e07bdba6c78092b4cbaf60de4562
ankitosh <ankitdigitek@gmail.com>
2019-12-25 12:39:21 +0300

    Variables Added

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "service_role_arn" {
+    default = "${var.service_role_arn}"
+}
+
+variable "secret_key" {
+    default = "${var.secret_key}"
+}
+
+variable "access_key" {
+    default = "${var.access_key}"
+}
