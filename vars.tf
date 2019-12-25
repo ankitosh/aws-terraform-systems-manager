083b490466f6ba5a5a0b5a77016822ffe289257a
ankitosh <38682280+ankitosh@users.noreply.github.com>
2019-12-25 11:38:17 +0300

    update vars.tf

--- vars.tf
+++ vars.tf
@@ -1,15 +1,17 @@
 
-data "aws_caller_identity" "current" {}
+#data "aws_caller_identity" "current" {}
 provider "aws" {
+  access_key  = "${var.access_key}"
+  secret_key  = "${var.secret_key}"
   region     = "eu-west-1"
 
 }
-terraform {
+# terraform {
 
-  backend "s3" {
-    bucket        = "YOUR-BUCKET-NAME-tf-state-dev"
-    key           = "patch-mgmt.tfstate"
-    region        = "eu-west-1"
-    encrypt       = true
-  }
-}
+#   backend "s3" {
+#     bucket        = "YOUR-BUCKET-NAME-tf-state-dev"
+#     key           = "patch-mgmt.tfstate"
+#     region        = "eu-west-1"
+#     encrypt       = true
+#   }
+#}
