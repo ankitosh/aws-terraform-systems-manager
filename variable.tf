413128140d944d4b76822694472d0465f15db828
ankitosh <38682280+ankitosh@users.noreply.github.com>
2019-12-25 11:40:06 +0300

    update variable

--- variable.tf
+++ variable.tf
@@ -1,3 +1,11 @@
 variable "service_role_arn" {
     default = "arn:aws:iam::YOUR-ACCOUNT-ID:role/AutomationServiceRole"
 }
+
+variable "access_key"
+default = "${ var.access_key}"
+}
+
+variable "secret_key" {
+    default = "${var.secret_key}"
+ }
